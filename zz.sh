0f8d41eabc7ab912768d77110002831a9327f7da
qianyuqiao <18302996874@163.com>
2018-12-14 23:17:04 +0800

    zz.sh
     --sh code to add your blog access number

---
+++ zz.sh
@@ -0,0 +1,6 @@
+pip install selenium
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar -xvf phantomjs-2.1.1-linux-x86_64
+cp   ./phantomjs-2.1.1-linux-x86_64/bin/phantomjs   /usr/bin/phantomjs
+wget https://raw.githubusercontent.com/qianyuqiao/scripts/master/access.py
+python access.py

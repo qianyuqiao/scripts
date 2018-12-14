1db39a0cdfe0bc57a9a40822c195b7b0425f8f4b
qianyuqiao <18302996874@163.com>
2018-12-15 00:10:19 +0800

    Update zz.sh

--- zz.sh
+++ zz.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
 pip install selenium
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
-tar -xvf phantomjs-2.1.1-linux-x86_64
+url="https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2"
+wget  "${url}"
+binary="phantomjs-2.1.1-linux-x86_64.tar.bz2"
+tar -xvf ${binary}
 cp   ./phantomjs-2.1.1-linux-x86_64/bin/phantomjs   /usr/bin/phantomjs
 wget https://raw.githubusercontent.com/qianyuqiao/scripts/master/access.py
 python access.py
